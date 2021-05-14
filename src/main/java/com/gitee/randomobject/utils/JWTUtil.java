ccd60c02a1ce780a84ae4fc45323599222e3dc37
randomobject <nikolaperelman@outlook.com>
2021-05-14 15:22:07 +0800

    增加jwt自定义key-value的方式生成token

--- src/main/java/com/gitee/randomobject/utils/JWTUtil.java
+++ src/main/java/com/gitee/randomobject/utils/JWTUtil.java
@@ -1,27 +1,29 @@
 package com.gitee.randomobject.utils;
 
+import cn.hutool.core.util.ObjectUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.gitee.randomobject.domain.Payload;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Map;
 import java.util.UUID;
 
 /**
  * jwtutis工具类
  */
+@Slf4j
 public class JWTUtil {
 
 
-    private static final String JWT_KEY = "user";
+    private static String JWT_KEY = "user";
 
     /**
      * 私钥加密token
+     *
      * @param userInfo   待加密数据，即载荷中的数据
      * @param privateKey 私钥
      * @param expire     过期时间 /分钟
@@ -32,6 +34,35 @@ public class JWTUtil {
         return Jwts.builder()
                 .claim(JWT_KEY, JSONObject.toJSONString(userInfo))
                 .setId(createJID())
+                .setExpiration(DateTime.now().plusMinutes(expire).toDate())
+                .signWith(privateKey, SignatureAlgorithm.RS256)
+                .compact();
+    }
+
+    /**
+     * 自定义JWT_KEY
+     * @param key 自定义的key
+     */
+    public static void setJwtKey(String key) {
+        JWT_KEY = key;
+    }
+
+    /**
+     * 自定义key和value 私钥加密token
+     *
+     * @param keyAndValue 使用自定义的key来取加密的值
+     * @param privateKey  私钥
+     * @param expire      过期时间 /分钟
+     * @return jwt
+     */
+    public static String generateToken(Map<String, Object> keyAndValue, PrivateKey privateKey, int expire) {
+
+        if (ObjectUtil.isEmpty(keyAndValue)) {
+            throw new IllegalArgumentException("自定义的keyAndValue不能为null");
+        }
+        JwtBuilder builder = Jwts.builder();
+        keyAndValue.forEach((k, v) -> builder.claim(k, v));
+        return builder.setId(createJID())
                 .setExpiration(DateTime.now().plusMinutes(expire).toDate())
                 .signWith(privateKey, SignatureAlgorithm.RS256)
                 .compact();
@@ -51,6 +82,7 @@ public class JWTUtil {
 
     /**
      * 获取token中的(returnInfo)返回信息
+     *
      * @param token     用户请求中的令牌
      * @param publicKey 公钥
      * @param classType 返回class类型
@@ -72,6 +104,7 @@ public class JWTUtil {
 
     /**
      * 获取token中的载荷信息，不返回（returnInfo）返回信息
+     *
      * @param token     用户请求中的令牌
      * @param publicKey 公钥
      * @param <T>
@@ -92,11 +125,9 @@ public class JWTUtil {
 
     /**
      * 创建JID
-     *
      * @return String JID
      */
     public static String createJID() {
-
         return UUID.randomUUID().toString().replaceAll("-", "");
     }
 
